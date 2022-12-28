941d9ee995ad4839776fe1b2e85fda71f85bfd43
yangtaehyuck <cjddk0920@gmail.com>
2022-12-28 22:25:12 +0900

    33

--- 문자열내림차순으로배치하기.js
+++ 문자열내림차순으로배치하기.js
@@ -1,4 +1,15 @@
-function solution(s) {
-  var answer = '';
-  return answer;
+function solution(a, b) {
+  let sum = 0;
+  if (a < b) {
+    for (i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else if (a === b) {
+    sum = a;
+  } else {
+    for (i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum
 }
