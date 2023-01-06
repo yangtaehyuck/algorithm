c6ef3bc2c811fa80eda62bb0f6dc87739b419cf7
yangtaehyuck <cjddk0920@gmail.com>
2023-01-06 23:09:40 +0900

    39

---
+++ 소수만들기.js
@@ -0,0 +1,8 @@
+function solution(seoul) {
+  for (i = 0; i <= seoul.length; i++) {
+    if (seoul[i] === 'Kim') {
+      return `김서방은 ${i}에 있다`
+    }
+  }
+}
+console.log(solution(["Jane", "Kim"]))
