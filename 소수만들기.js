6617d41786b17c3c45cd189a9cf526620ba6d794
yangtaehyuck <cjddk0920@gmail.com>
2023-01-08 22:39:29 +0900

    41

--- 소수만들기.js
+++ 소수만들기.js
@@ -12,3 +12,9 @@ function findKim(seoul){
   var idx = seoul.indexOf('Kim');
   return "김서방은 " + idx + "에 있다";
 }
+
+const solution = (arr) => `김서방은 ${arr.findIndex(s => s === 'Kim')}에 있다`;
+
+function findKim(seoul) {
+  return "김서방은 " + seoul.indexOf('Kim') + "에 있다";
+}
